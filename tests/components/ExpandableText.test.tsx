9005f5092ebd061a365c1ca7d41fb90d76007c33
njmlopez17 <njmlopez@yahoo.com>
2024-11-20 17:30:55 -0800

    updated files

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -4,6 +4,9 @@ import ExpandableText from '../../src/components/ExpandableText';
 import userEvent from '@testing-library/user-event';
 
 describe ('ExpandableText', () => {
+    const limit = 255;
+    const longText = 'a'.repeat (limit + 1);
+    const truncatedText = longText.substring (0, limit) + "...";
 
     /* Test Case 01 */
     /* Expected result is it should render the full text if less than 25 char */
@@ -18,9 +21,7 @@ describe ('ExpandableText', () => {
     /* Expected result is it should truncate the text if more than 255 char */
     it('should truncate the text if more than 255 char', () => {
             // this is to check the text if more than 255 char
-            const text = 'a'.repeat(256);
-            render(<ExpandableText text={text} />);
-            const truncatedText = text.substring(0, 255) + '...';
+            render(<ExpandableText text={longText} />);
             expect(screen.getByText(truncatedText)).toBeInTheDocument();
             // this is to check the expected buttons
             const button = screen.getByRole('button');
@@ -32,15 +33,34 @@ describe ('ExpandableText', () => {
     /* Expected result is it should expand text when Show More button is clicked */
     it('should expand text when Show More button is clicked', async () => {
         // this is to check the text if more than 255 char...
-         const text = 'a'.repeat(256);
-        render(<ExpandableText text={text} />);
+        render(<ExpandableText text={longText} />);
         // this is to check when button is clicked...
         const button = screen.getByRole('button');
         const user = userEvent.setup();
         await user.click(button);
         // this is to check the expected buttons...
-        expect(screen.getByText(text)).toBeInTheDocument();
-                expect(button).toHaveTextContent(/less/i);
+        expect(screen.getByText(longText)).toBeInTheDocument();
+        expect(button).toHaveTextContent(/less/i);
+    console.log('PASSED as expected.');
+    })
+
+    /* Test Case 04 */
+    /* Expected result is it should collapse text when Show Less button is clicked */
+    it('should collapse text when Show Less button is clicked', async () => {
+        // this is to check the text if more than 255 char...
+        render(<ExpandableText text={longText} />);
+
+        // this is to check when button is clicked the first time (button is from Show More to Show Less)...
+        const showMoreButton = screen.getByRole('button', {name: /more/i});
+        const user = userEvent.setup();
+        await user.click(showMoreButton);
+        // this is to check when button is clicked the after full text is displayed and button is from Show Less to Show More again)...
+        const showLessButton = screen.getByRole('button', {name: /less/i});
+        await user.click(showLessButton);
+
+        // this is to check the expected buttons...
+        expect(screen.getByText(truncatedText)).toBeInTheDocument();
+        expect(showMoreButton).toHaveTextContent(/more/i);
     console.log('PASSED as expected.');
     })
 })
--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -1,3 +1,4 @@
+/* Import functions and libraries */
 import { render, screen } from '@testing-library/react';
 import TermsAndConditions from '../../src/components/TermsAndConditions';
 import userEvent from '@testing-library/user-event';
@@ -5,35 +6,37 @@ import userEvent from '@testing-library/user-event';
 /* Test Case 01 */
 /* Expected result is it should render with correct text and initial state */
 describe ('TermsAndConditions', () => {
+    const renderComponent = () => {
+        render(<TermsAndConditions/>)
+        return{
+            heading: screen.getByRole('heading'),
+            checkbox: screen.getByRole ('checkbox'),
+            button: screen.getByRole ('button')
+        }
+    } 
     it('should render with correct text and initial state', () => {
-        render(<TermsAndConditions/>);
-        const heading = screen.getByRole('heading');
-            expect(heading).toBeInTheDocument();
+        const {heading, checkbox, button} = renderComponent();
+
             expect(heading).toHaveTextContent ('Terms & Conditions');
         // validate the checkbox's initial state is unchecked
-        const checkbox = screen.getByRole ('checkbox');
-            expect(checkbox).toBeInTheDocument();
             expect(checkbox).not.toBeChecked();
         // validate the Submit button is initially disabled
-        const button = screen.getByRole ('button');
-            expect(button).toBeInTheDocument();
-            expect(button).toHaveTextContent(/submit/i);
             expect(button).toBeDisabled();
         console.log('PASSED as expected.');
-    })
+    });
 
 /* Test Case 02 */
 /* Expected result is it should enable the button when the checkbox is checked */
 // Note: install user-event library from @testing-library (this is to simullate a user behavior from the UI)
     it('should enable the button when the checkbox is checked', async () => {
+        const {checkbox, button} = renderComponent();
+        
         //Arrange
-        render(<TermsAndConditions/>);
         //Act
-        const checkbox = screen.getByRole ('checkbox');
         const user = userEvent.setup();
-            await user.click(checkbox);
+        await user.click(checkbox);
         //Assert
-            expect(screen.getByRole('button')).toBeEnabled();   
+        expect(button).toBeEnabled();   
         console.log('PASSED as expected.');
     })  
 })
